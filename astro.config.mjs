6c867ef7146716b95d1ba2ace1b0e6678cb5126a
Panagiotis Pavlidis <panpavlidis98@gmail.com>
2026-04-15 23:43:19 +0300

    Improve SEO

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,12 @@
 import { defineConfig } from 'astro/config';
+import sitemap from '@astrojs/sitemap';
 
 export default defineConfig({
   site: 'https://panos-ece.github.io',
   base: '/panagiotis_pavlidis',
+  integrations: [
+    sitemap({
+      filter: (page) => page !== 'https://panos-ece.github.io/panagiotis_pavlidis',
+    }),
+  ],
 });
